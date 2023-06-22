c30faebc716319135be9f9dbee89a777ed6e890e
Theo McCray <tlmccraycray@gmail.com>
2023-06-22 11:07:33 -0600

    Add methods

--- arrays.js
+++ arrays.js
@@ -1,3 +1,15 @@
 var greetings = ["hello", "howdy", "hi", "sup"];
 var oddNumbers = [1, 3, 5, 7];
 var boolArray = [true, true, false, true];
+
+//this line should reverse the array
+greetings.reverse();
+console.log(greetings);
+
+//this should add the value to the array
+oddNumbers.push(9);
+console.log(oddNumbers);
+
+//this should erase the last element of the array
+boolArray.pop();
+console.log(boolArray);
